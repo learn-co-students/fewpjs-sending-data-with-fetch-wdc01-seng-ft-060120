a61d869c81340d1696a0d0fbee5033d8e1443dd4
steve <stevepesce879@gmail.com>
2020-07-15 23:47:52 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,70 @@
 // Add your code here
+let configurationObject = 
+{
+  method: "POST",
+  headers: 
+  {
+    "Content-Type": "application/json",
+    "Accept": "application/json"
+  },
+  body: JSON.stringify(
+    {
+      dogName: "Byron",
+      dogBreed: "Poodle"
+    }
+  )
+};
+
+fetch("http://localhost:3000/dogs",configurationObject)
+.then(function(response)
+  {
+    return response.json();
+  }
+).then(function(object)
+  {
+    console.log(object);
+  }
+).catch(function(error)
+  {
+    alert("Bad things! Ragnarok!");
+    console.log(error.message);
+  }
+);
+
+function submitData(name,email)
+{
+  const jsonObj = 
+  {
+    method: "POST",
+    headers: 
+    {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify(
+      {
+        name: name,
+        email: email
+      }
+    )
+  };
+  const body = document.getElementsByTagName('body')[0];
+  return fetch("http://localhost:3000/users",jsonObj)
+  .then(function(response)
+    {
+      return response.json();
+    }
+  ).then(function(object)
+    {
+      const p = document.createElement('p');
+      p.innerHTML = object.id;
+      body.append(p);
+    }
+  ).catch(function(error)
+    {
+      const p = document.createElement('p');
+      p.innerHTML = error.message;
+      body.append(p);
+    }
+  );
+}
