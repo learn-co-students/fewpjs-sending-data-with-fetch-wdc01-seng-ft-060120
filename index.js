53ef8b3c91dc1aa380268663fba93fc0d43e9747
Catrina Friday <cat.friday@gmail.com>
2020-07-18 21:32:23 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,63 @@
+// const { default: fetch } = require("node-fetch");
+
 // Add your code here
+
+// let configObj = {
+//     headers: {
+//         "Content-Type": "application/json",
+//         "Accept": "application/json"
+//       },
+//       body: JSON.stringify({
+//           name,
+//           email
+//       })
+// };
+
+
+// function submitData(name, email) {
+//    return fetch('http://localhost:3000/users', {
+//        method: "POST",
+//      headers: {
+//         "Content-Type": "application/json",
+//         "Accept": "application/json"
+//       },
+//       body: JSON.stringify({
+//           name,
+//           email
+//       })
+//    })
+//     .then(function(respose) {
+//         return response.json()
+//     })
+//     .then(function(object){
+//         document.body.innerHTML = object[ "id" ]
+//     })
+//     .catch(function(error) {
+//         document.body.innerHTML = error.message
+//       })
+// }
+// submitData
+
+function submitData( name, email ) {
+    return fetch( 'http://localhost:3000/users', {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+        },
+        body: JSON.stringify( {
+          name,
+          email
+        } )
+      } )
+      .then( function ( response ) {
+        return response.json()
+      } )
+      .then( function ( object ) {
+        document.body.innerHTML = object[ "id" ]
+      } )
+      .catch( function ( error ) {
+        document.body.innerHTML = error.message
+      } )
+  }
+  
