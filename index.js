7c523c65b989aee33c3f8c2b3458a33f40dc344f
andy-aguilar <aaguil3@gmail.com>
2020-07-19 11:03:33 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Add your code here
+const submitData = (userName, userEmail) => {
+    let configUser = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            name: userName,
+            email: userEmail
+        })
+    };
+    return fetch("http://localhost:3000/users", configUser)
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(object){
+        document.body.innerHTML = object["id"]
+        
+    })
+    .catch(function(error) {
+       document.body.innerHTML = error.message
+    })
+    
+}
+
+
+submitData()
+
+
+
